dbe7f055099e18f3861147e8c05e57d0ac965dd0
Raditya Prazenko <5026241155@student.its.ac.id>
2025-06-23 12:41:20 +0700

    Initial commit

--- src/TTTGraphicsSoundImage/Seed.java
+++ src/TTTGraphicsSoundImage/Seed.java
@@ -17,8 +17,8 @@ import javax.swing.ImageIcon;
  *  however, not supported.
  */
 public enum Seed {   // to save as "Seed.java"
-    CROSS("X", "images/cross.gif"),   // displayName, imageFilename
-    NOUGHT("O", "images/not.gif"),
+    CROSS("PACMAN", "images/PacmanOKE.png"),   // displayName, imageFilename
+    NOUGHT("GHOST", "images/GhostOKE.png"),
     NO_SEED(" ", null);
 
     // Private variables
